c4236e0889d7ff03e00e30f4d6cd1921189e0aa5
RHAIREN Pc <135435739+Adrriian@users.noreply.github.com>
2025-03-18 20:20:46 +0000

    asasd

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,15 @@ let number2 = "";
 let operador = "";
 let marcador = "";
 
-let btn = documentQuerySelector(".key");
+let btn = document.querySelector(".key");
 let result = document.querySelector(".result")
 
 btn.forEach(function(btns){
-    btns.addEventListener("click", function(){
+    btns.addEventListener("click")
         switch(btns.innerText){
             case "1":
             result.innerText = 1;
             break;
-        }
-    })
+        
+    }
 })
