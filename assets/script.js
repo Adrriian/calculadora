a105c0cc4ead2c9ad96c4f1ec32c2da44b3b73eb
RHAIREN Pc <135435739+Adrriian@users.noreply.github.com>
2025-03-19 14:48:18 +0000

    ataulziando tudo

--- assets/script.js
+++ assets/script.js
@@ -50,7 +50,7 @@ btn.forEach(function (btns) {
                 sinal()
                 break;
         }
-
+        }
 
         switch (btns.innerText) {
             case "1":; case "2":; case "3":; case "4":; case "5":; case "6":; case"7":; case"8":; case "9":;
