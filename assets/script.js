31e17e6ed8df27e24df14767d7784a8a158ba169
RHAIREN Pc <135435739+Adrriian@users.noreply.github.com>
2025-03-19 13:59:43 +0000

    arrumando calculadora

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ let marcador = 1;
 
 let btn = document.querySelectorAll(".key");
 let result = document.querySelector(".result")
+let calculoresult = "";
 
 btn.forEach(function (btns) {
     btns.addEventListener("click", function () {
@@ -19,19 +20,43 @@ btn.forEach(function (btns) {
             }
         }
 
+
         switch (btns.innerText) {
-            case "1":;
-            case "2":
+            case "1":;case "2";case: "3";case "4"; case "5"; case "6"; case"7"; case"8"; case "9";
+            case "0":
             colocandonumero()
-                break; 
+                break;
+
             case "+": 
-            operador = "+"
-            console.log(`sinal é: ` + operador)
+            operador = btns.innerText
+            calculoresult = Number(number) + Number(number2)
             break;
+
             case "-": 
-          
-            operador = "-"
-            console.log(`sinal é: ` + operador)
+            operador = btns.innerText
+            calculoresult = Number(number) - Number(number2)
+            break;
+
+            case "*": 
+            operador = btns.innerText
+            calculoresult = Number(number) * Number(number2)
+            break;
+
+            case "/": 
+            operador = btns.innerText
+            calculoresult = Number(number) / Number(number2)
+            break;
+
+            case "C":
+                let number = "";
+                let number2 = "";
+                let operador = "";
+                let marcador = 1;
+                result.innerText = 0
+            break;
+
+            case "=":
+                result.innerText = calculoresult
             break;
         }
 
