623f9ecae7b6a23b72676a40c68be7e16d86f543
Adrian <adrianribeiroprofissional@gmail.com>
2025-03-19 08:06:55 -0300

    asd

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+let number = "";
+let number2 = "";
+let operador = "";
+let marcador = 1;
+
+let btn = document.querySelectorAll(".key");
+let result = document.querySelector(".result")
+
+btn.forEach(function (btns) {
+    btns.addEventListener("click", function () {
+
+        switch (btns.innerText) {
+            case "1":
+                number = btns.innerText
+                console.log(`numero 1 é: ` + number)
+                break; s
+        }
+
+        update()
+    })
+})
+
+function update() {
+    if (number === "") {
+        result.innerText = "2"
+    } else {
+        result.innerText = number + operador + number2
+    }
+}
